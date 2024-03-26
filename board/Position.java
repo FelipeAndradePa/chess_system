2e0765f192ab50b3ee7e8d74a4669a7e3b761bda
Felipe Andrade <91270824+FelipeAndradePa@users.noreply.github.com>
2024-03-26 16:11:28 -0300

    board Layer

---
+++ board/Position.java
@@ -0,0 +1,38 @@
+package board;
+
+public class Position {
+
+    private int column;
+    private int row;
+
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public void setValues(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    @Override
+    public String toString() {
+        return row + ", " + column;
+    }
+}
